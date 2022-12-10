ae7c343dd1c25ff60978537a8884799ea73cd63d
Leon Thompson <ltyronethompson09@gmail.com>
2022-12-10 06:50:54 -0600

    adding basic movement animations

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -3,12 +3,22 @@
 
 #include "ShooterCharacter.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+
 // Sets default values
 AShooterCharacter::AShooterCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm Component"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Component"));
+	Camera->SetupAttachment(SpringArm);
 }
 
 // Called when the game starts or when spawned
@@ -30,5 +40,47 @@ void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AShooterCharacter::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AShooterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &AShooterCharacter::LookRight);
+	PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &AShooterCharacter::LookUpRate);
+	PlayerInputComponent->BindAxis(TEXT("LookRightRate"), this, &AShooterCharacter::LookRightRate);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &AShooterCharacter::Jump);
 }
+
+void AShooterCharacter::MoveForward(float AxisValue)
+{
+	// Already framerate independent
+	AddMovementInput(GetActorForwardVector() * AxisValue * ForwardSpeed);
+}
+
+void AShooterCharacter::MoveRight(float AxisValue)
+{
+	// Already framerate independent
+	AddMovementInput(GetActorRightVector() * AxisValue * SideSpeed);
+}
+
+void AShooterCharacter::LookRight(float AxisValue)
+{
+	AddControllerYawInput(AxisValue * MouseHorizontalSpeed * UGameplayStatics::GetWorldDeltaSeconds(this));
+}
+
+void AShooterCharacter::LookUp(float AxisValue)
+{
+	AddControllerPitchInput(AxisValue * MouseVerticalSpeed * UGameplayStatics::GetWorldDeltaSeconds(this));
+}
+
+void AShooterCharacter::LookRightRate(float AxisValue)
+{
+	AddControllerYawInput(AxisValue * GamepadLookSpeed * UGameplayStatics::GetWorldDeltaSeconds(this));
+}
+
+void AShooterCharacter::LookUpRate(float AxisValue)
+{
+	AddControllerPitchInput(AxisValue * GamepadLookSpeed * UGameplayStatics::GetWorldDeltaSeconds(this));
+}
+
+
 
--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -19,6 +19,13 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCameraComponent* Camera;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -26,4 +33,29 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookRight(float AxisValue);
+	void LookUp(float AxisValue);
+
+	void LookRightRate(float AxisValue);
+	void LookUpRate(float AxisValue);
+
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float ForwardSpeed = 100.f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float SideSpeed = 100.f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MouseVerticalSpeed = 50.f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MouseHorizontalSpeed = 50.f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float GamepadLookSpeed = 50.f;
 };
